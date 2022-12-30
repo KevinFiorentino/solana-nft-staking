bb5480412a116d1a2acb01e6301617c7fa8f4c3b
Kevin <kevinfiorentino.1294@gmail.com>
2022-12-30 11:28:21 -0300

    Dependencies and program struct

--- programs/solana-nft-staking/src/lib.rs
+++ programs/solana-nft-staking/src/lib.rs
@@ -1,4 +1,14 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke_signed;
+use anchor_spl::token;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Approve, Mint, MintTo, Revoke, Token, TokenAccount},
+};
+use mpl_token_metadata::{
+    instruction::{freeze_delegated_account, thaw_delegated_account},
+    ID as MetadataTokenId,
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +16,24 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_nft_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn redeem(ctx: Context<Redeem>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Stake {}
+
+#[derive(Accounts)]
+pub struct Redeem {}
+
+#[derive(Accounts)]
+pub struct Unstake {}
