dfe527cc8a2e9481b0edbcaf8b71860843bec588
Kevin <kevinfiorentino.1294@gmail.com>
2022-12-30 11:33:10 -0300

    Add structs and impl to program

--- programs/solana-nft-staking/src/lib.rs
+++ programs/solana-nft-staking/src/lib.rs
@@ -37,3 +37,25 @@ pub struct Redeem {}
 
 #[derive(Accounts)]
 pub struct Unstake {}
+
+#[account]
+pub struct UserStakeInfo {
+    pub token_account: Pubkey,
+    pub stake_start_time: i64,
+    pub last_stake_redeem: i64,
+    pub user_pubkey: Pubkey,
+    pub stake_state: StakeState,
+    pub is_initialized: bool,
+}
+
+#[derive(Debug, PartialEq, AnchorDeserialize, AnchorSerialize, Clone)]
+pub enum StakeState {
+    Unstaked,
+    Staked,
+}
+
+impl Default for StakeState {
+    fn default() -> Self {
+        StakeState::Unstaked
+    }
+}
