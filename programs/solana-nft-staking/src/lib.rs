a19e26644379fb7fa37df49084611fff9092e5b4
Kevin <kevinfiorentino.1294@gmail.com>
2022-12-30 13:37:24 -0300

    Staking redeem instruction

--- programs/solana-nft-staking/src/lib.rs
+++ programs/solana-nft-staking/src/lib.rs
@@ -68,6 +68,52 @@ pub mod solana_nft_staking {
     }
 
     pub fn redeem(ctx: Context<Redeem>) -> Result<()> {
+        require!(
+            ctx.accounts.stake_state.is_initialized,
+            StakeError::UninitializedAccount
+        );
+
+        require!(
+            ctx.accounts.stake_state.stake_state == StakeState::Staked,
+            StakeError::InvalidStakeState
+        );
+
+        let clock = Clock::get()?;
+
+        msg!(
+            "Stake last redeem: {:?}",
+            ctx.accounts.stake_state.last_stake_redeem
+        );
+
+        msg!("Current time: {:?}", clock.unix_timestamp);
+        let unix_time = clock.unix_timestamp - ctx.accounts.stake_state.last_stake_redeem;
+        msg!("Seconds since last redeem: {}", unix_time);
+        let redeem_amount = (10 * i64::pow(10, 2) * unix_time) / (24 * 60 * 60);
+        msg!("Elligible redeem amount: {}", redeem_amount);
+
+        msg!("Minting staking rewards");
+        token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.stake_mint.to_account_info(),
+                    to: ctx.accounts.user_stake_ata.to_account_info(),
+                    authority: ctx.accounts.stake_authority.to_account_info(),
+                },
+                &[&[
+                    b"mint".as_ref(),
+                    &[*ctx.bumps.get("stake_authority").unwrap()],
+                ]],
+            ),
+            redeem_amount.try_into().unwrap(),
+        )?;
+
+        ctx.accounts.stake_state.last_stake_redeem = clock.unix_timestamp;
+        msg!(
+            "Updated last stake redeem time: {:?}",
+            ctx.accounts.stake_state.last_stake_redeem
+        );
+
         Ok(())
     }
 
@@ -107,7 +153,39 @@ pub struct Stake<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Redeem {}
+pub struct Redeem<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        token::authority=user
+    )]
+    pub nft_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [user.key().as_ref(), nft_token_account.key().as_ref()],
+        bump,
+        constraint = *user.key == stake_state.user_pubkey,
+        constraint = nft_token_account.key() == stake_state.token_account
+    )]
+    pub stake_state: Account<'info, UserStakeInfo>,
+    #[account(mut)]
+    pub stake_mint: Account<'info, Mint>,
+    /// CHECK: manual check
+    #[account(seeds = ["mint".as_bytes().as_ref()], bump)]
+    pub stake_authority: UncheckedAccount<'info>,
+    #[account(
+        init_if_needed,
+        payer=user,
+        associated_token::mint=stake_mint,
+        associated_token::authority=user
+    )]
+    pub user_stake_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
 #[derive(Accounts)]
 pub struct Unstake {}
--- tests/solana-nft-staking.ts
+++ tests/solana-nft-staking.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { getAccount } from "@solana/spl-token"
 import { SolanaNftStaking } from "../target/types/solana_nft_staking";
 import { PROGRAM_ID as METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
 import { setupNft } from './utils/setupNft';
@@ -39,6 +40,21 @@ describe("Solana NFT Staking", () => {
 
     const account = await program.account.userStakeInfo.fetch(stakeStatePda)
     expect(account.stakeState === "Staked")
+  })
+
+  it("Redeems", async () => {
+    await program.methods
+      .redeem()
+      .accounts({
+        nftTokenAccount: nft.tokenAddress,
+        stakeMint: mint,
+        userStakeAta: tokenAddress,
+      })
+      .rpc()
+
+    const account = await program.account.userStakeInfo.fetch(stakeStatePda)
+    expect(account.stakeState === "Unstaked")
+    const tokenAccount = await getAccount(provider.connection, tokenAddress)
   })
 
 });
