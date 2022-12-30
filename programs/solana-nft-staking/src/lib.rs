e1bcfee59ddc17f8264b15b0880af56c4cd2598d
Kevin <kevinfiorentino.1294@gmail.com>
2022-12-30 11:26:51 -0300

    Revert "Dependencies and basic conde"
    This reverts commit 1d32c44e9563d265c0870c632e46e33d2a426822.

--- programs/solana-nft-staking/src/lib.rs
+++ programs/solana-nft-staking/src/lib.rs
@@ -1,14 +1,4 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::program::invoke_signed;
-use anchor_spl::token;
-use anchor_spl::{
-    associated_token::AssociatedToken,
-    token::{Approve, Mint, MintTo, Revoke, Token, TokenAccount},
-};
-use mpl_token_metadata::{
-    instruction::{freeze_delegated_account, thaw_delegated_account},
-    ID as MetadataTokenId,
-};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -16,24 +6,10 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_nft_staking {
     use super::*;
 
-    pub fn stake(ctx: Context<Stake>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn redeem(ctx: Context<Redeem>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Stake {}
-
-#[derive(Accounts)]
-pub struct Redeem {}
-
-#[derive(Accounts)]
-pub struct Unstake {}
+pub struct Initialize {}
